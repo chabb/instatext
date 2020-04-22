ef4a2d3b00f77784061bc92ff80a89c18dc60baa
Francois Chabbey <francois@streamsets.com>
2020-04-22 16:24:03 +0200

    Initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
+import './App.less';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Layout, Menu, Button} from "antd";
+import { UploadOutlined, UserOutlined, VideoCameraOutlined, PlusOutlined } from '@ant-design/icons';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout>
+        <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={broken => {
+              console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+              console.log(collapsed, type);
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="logo">Instatext</div>
+          <div className='main-nav'>
+            <Button className='add-message' type="primary" shape="round" icon={<PlusOutlined />}>
+              Add new message
+            </Button>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <UserOutlined />
+                <span className="nav-text">Inbox</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <VideoCameraOutlined />
+                <span className="nav-text">Contacts</span>
+              </Menu.Item>
+            </Menu>
+          </div>
+          <div className='bottom-nav'>
+            Manage Account
+          </div>
+        </Sider>
+        <Layout>
+          <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Manage Account
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
   );
 }
 
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export interface Message {
+    recipients: string[];
+    message: string;
+    timestamp: number;
+}
+
+
+
+// DB -> a users

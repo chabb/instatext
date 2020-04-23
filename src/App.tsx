9a7317bfd47929be5ab0aefc4d7591fc9c2bc777
Francois Chabbey <francois@streamsets.com>
2020-04-23 21:29:04 +0200

    Add basic behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.less';
-import React from 'react';
-import {Layout, Menu, Button} from "antd";
+import React, {useState} from 'react';
+import {Layout, Menu, Button, Modal} from "antd";
 import { UploadOutlined, UserOutlined, VideoCameraOutlined, PlusOutlined } from '@ant-design/icons';
 import {
     BrowserRouter as Router,
@@ -15,66 +15,76 @@ const Home = () => <span> Home</span>;
 const Contact = () => <span> Home</span>;
 
 function App() {
-  return (
-      <Router>
-          <Layout>
-              <Sider
-                  breakpoint="lg"
-                  collapsedWidth="0"
-                  onBreakpoint={broken => {
-                      console.log(broken);
-                  }}
-                  onCollapse={(collapsed, type) => {
-                      console.log(collapsed, type);
-                  }}
-              >
-                  <div className="logo">Instatext</div>
-                  <div className='main-nav'>
-                      <Button className='add-message' type="primary" shape="round" icon={<PlusOutlined />}>
-                          Add new message
-                      </Button>
-                      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                              <Menu.Item key="1">
-                                  <Link to='/inboc'>
-                                      <UserOutlined />
-                                      <span className="nav-text">Inbox</span>
-                                  </Link>
-                              </Menu.Item>
-                              <Menu.Item key="2">
-                                  <Link to='/contact'>
-                                      <VideoCameraOutlined />
-                                      <span className="nav-text">Contacts</span>
-                                  </Link>
-                              </Menu.Item>
-                      </Menu>
-                  </div>
-                  <Link to='/admin'>
-                      <div className='bottom-nav'>
-                          Manage Account
-                      </div>
-                  </Link>
-              </Sider>
-              <Layout>
-                  <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-                  <Content style={{ margin: '24px 16px 0' }}>
-                      <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                          <Switch>
-                              <Route path="/inbox"/>
-                                    <Inbox/>
-                              <Route path="/contact">
-                                  <Contact />
-                              </Route>
-                              <Route path="/">
-                                  <Home />
-                              </Route>
-                          </Switch>
-                      </div>
-                  </Content>
-                  <Footer style={{ textAlign: 'center' }}>I.T System Prod M.C D.C ©2021</Footer>
-              </Layout>
-          </Layout>
-      </Router>
-  );
+    const [modalVisible, setModalVisible] = useState(false);
+    return (
+        <Router>
+            <Layout>
+                <Sider
+                    breakpoint="lg"
+                    collapsedWidth="0"
+                    onBreakpoint={broken => {
+                        console.log(broken);
+                    }}
+                    onCollapse={(collapsed, type) => {
+                        console.log(collapsed, type);
+                    }}
+                >
+                    <div className="logo">Instatext</div>
+                    <div className='main-nav'>
+                        <Button onClick={() => setModalVisible(true)}
+                            className='add-message' type="primary" shape="round" icon={<PlusOutlined />}>
+                            Add new message
+                        </Button>
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1">
+                                <Link to='/inboc'>
+                                    <UserOutlined />
+                                    <span className="nav-text">Inbox</span>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Link to='/contact'>
+                                    <VideoCameraOutlined />
+                                    <span className="nav-text">Contacts</span>
+                                </Link>
+                            </Menu.Item>
+                        </Menu>
+                    </div>
+                    <Link to='/admin'>
+                        <div className='bottom-nav'>
+                            Manage Account
+                        </div>
+                    </Link>
+                </Sider>
+                <Layout>
+                    <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+                    <Content style={{ margin: '24px 16px 0' }}>
+                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                            <Switch>
+                                <Route path="/inbox"/>
+                                <Inbox/>
+                                <Route path="/contact">
+                                    <Contact />
+                                </Route>
+                                <Route path="/">
+                                    <Home />
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>I.T System Prod M.C D.C ©2021</Footer>
+                </Layout>
+            </Layout>
+            <Modal
+                title="Send a new message"
+                visible={modalVisible}
+                onOk={() => setModalVisible(false)}
+                onCancel={() => setModalVisible(false)}
+            >
+                <p>Form todo</p>
+            </Modal>
+        </Router>
+    );
 }
 
 
--- src/inbox/inbox.tsx
+++ src/inbox/inbox.tsx
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react'
-import {Table, Input} from "antd";
+import {Table, Input, Drawer} from "antd";
 import {columns, generateFakeData} from "./message-table-definition";
 
 const { Search } = Input;
 export const Inbox = () => {
+    const [isDrawerVisible, setDrawerVisible] = useState(false);
+    const [selectedRow, setSelectedRow] = useState<any>(null);
+
     const [data, setData] = useState(generateFakeData(10));
     useEffect(() => { }, []); // potentially load there
     return (
@@ -13,7 +16,26 @@ export const Inbox = () => {
             enterButton="Search"
             size="large"
             onSearch={value => console.log(value)}/>
-        <Table columns={columns} dataSource={data} onChange={() => {}} />
+        <Table columns={columns}
+               onRow={(record) => ({
+                   onClick: () => {
+                       setSelectedRow(record);
+                       setDrawerVisible(true);
+                   },
+               })}
+               dataSource={data}
+               onChange={() => {}} />
+        <Drawer
+            title={`Message with ${selectedRow && selectedRow.recipients}`}
+            placement="right"
+            closable={false}
+            width={'40%'}
+            onClose={() => setDrawerVisible(false)}
+            visible={isDrawerVisible}
+        >
+            {selectedRow && selectedRow!.message}
+        </Drawer>
+
     </div>);
 }
 

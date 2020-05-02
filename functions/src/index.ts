d1d7c49968da2dde803668418355cb6d6c1c2853
Francois Chabbey <francois@streamsets.com>
2020-05-02 14:16:59 +0200

    WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,15 @@
 import * as functions from 'firebase-functions';
 import {Twilio} from 'twilio';
 import {MessageInstance, MessageStatus} from "twilio/lib/rest/api/v2010/account/message";
-const cors = require('cors')({
-    origin: true,
-});
+import {Collection} from "../../src/firebase/db";
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+import * as admin from 'firebase-admin';
+import FieldValue = admin.firestore.FieldValue;
+import cors from 'cors';
 
-
+const appCors = cors({origin:true});
+const db = admin.firestore();
+admin.initializeApp();
 
 /*Listen for changes in all documents in the 'users' collection
 exports.useWildcard = functions.firestore
@@ -22,11 +26,32 @@ exports.useWildcard = functions.firestore
         // change.after.data() == {name: "Marie"}
     });*/
 
+exports.createSubaccount = functions.https.onCall(({friendlyName, userId}, context): Promise<SubAccountResponse>  => {
+    //TODO(chab) lazy initialization of twilio client
+    console.log('create subaccount called');
+    const sid = functions.config().twilio.sid;
+    const token = functions.config().twilio.token;
+    const client = new Twilio(sid, token);
+    console.log('Create subaccount for', friendlyName, userId);
+    return client.api.accounts.create({friendlyName: 'Submarine'})
+        .then(account => {
+            console.log('created account', account);
+            return {subAccountId:account.sid}
+        }, (e) => {
+           console.log('Failed to create', e);
+            throw new functions.https.HttpsError('internal', 'Message:' + e)
+        });
+});
+
+interface SubAccountResponse {
+    subAccountId: string;
+}
 
 exports.sendMessage = functions.https.onCall(({to, from, message}, context): Promise<SendMessageResponse> => {
     console.log('start send message');
     const sid = functions.config().twilio.sid;
     const token = functions.config().twilio.token;
+    console.log(sid, token.slice(0, 5));
     if (!context.auth! || !context.auth!.uid) {
         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
             'while authenticated.');
@@ -38,9 +63,9 @@ exports.sendMessage = functions.https.onCall(({to, from, message}, context): Pro
     const textContent = {
         body: message,
         to: to,
-        from: from
+        from: from,
+        statusCallback: 'https://us-central1-instatext-27374.cloudfunctions.net/webhook'
     };
-    console.log('sending');
     return client.messages.create(textContent)
         .then((m: MessageInstance) => ({
                 from:m.from,
@@ -69,21 +94,42 @@ exports.webhook = functions.https.onRequest((req, res) => {
     // [END trigger]
     // [START sendError]
     // Forbidding PUT requests.
+    console.log('received request');
     if (req.method === 'GET' || req.method === 'PUT' || req.method === 'DELETE' ) {
         return res.status(403).send('Forbidden!');
     }
     // [END sendError]
     // [START usingMiddleware]
     // Enable CORS using the `cors` express middleware.
-    return cors(req, res, () => {
+    console.log('will response the thing', req.body);
+    //    SmsStatus: 'delivered',
+    //    MessageStatus: 'delivered',
+    //    To: '+41798843958',
+    //    MessageSid: 'SMf835763169154d9d9577ca3acec8ed84',
+    //    AccountSid: 'ACf4382285c8585fdabef1bc684075040c',
+    //    From: '+19145590987'
+
+
+    return appCors(req, res, () => {
         // [END usingMiddleware]
         // Reading date format from URL query parameter.
         // [START readQueryParam]
-        const from = req.body.from;
-        const status = req.body.status;
-        const message = req.body.sid;
-        console.log(from, status, message);
+        const from = req.body.From;
+        const status = req.body.Status;
+        const to = req.body.To;
+        const id = req.body.MessageSid;
+        //admin.firestore().db.collectionGroup('')
         // normal flow, sent -> delivered
+        db.collection(Collection.MESSAGES)
+            .doc(id).update({
+            lastStatusUpdate: FieldValue.serverTimestamp(),
+            status: status
+        }).then(() => {
+            console.log('status updated for ', id, from, to, status);
+        }, (e) => {
+          console.log('error updating status', e);
+        });
+
         res.status(200).send('hello');
         // [END sendResponse]
     });

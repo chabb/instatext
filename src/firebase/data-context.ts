ccda8c3bcd32a158435c3b38b76551d4a9acfd18
Francois Chabbey <francois@streamsets.com>
2020-05-10 21:54:51 +0200

    Update gitignore. Handle threads

--- src/firebase/data-context.ts
+++ src/firebase/data-context.ts
@@ -2,13 +2,19 @@ import {BehaviorSubject} from "rxjs";
 import Firebase from "./firebase";
 import {ITContact} from "../contacts/contact-table-definition";
 
+export enum MessageDirection {
+    INBOUND = 'inbound',
+    OUTBOUND = 'outbound'
+}
+
 export class Data {
 
     static instance;
     public contacts: BehaviorSubject<ITContact[]> = new BehaviorSubject([] as ITContact[]);
     public chats: BehaviorSubject<any[]> = new BehaviorSubject([] as any[]);
+    public chat: BehaviorSubject<any[]> = new BehaviorSubject([] as any[]);
+
     constructor(private fb: Firebase) {
-        console.log('INIT DATA CONTEXT');
         if (Data.instance || false) {
             console.log('FIXME !!!!!! Data callled two time');
             return Data.instance;
@@ -30,10 +36,52 @@ export class Data {
                     key: lastMessage.from,
                     from: lastMessage.from,
                     to: lastMessage.to,
+                    direction: lastMessage.direction,
                     message: lastMessage.message,
                     ts: lastMessage.createdAt})
             });
             this.chats.next(chats);
         })
     }
+
+    private chatSnapshotSub;
+
+    public startChatSnapshot(phoneNumber) {
+        this.chatSnapshotSub && this.chatSnapshotSub();
+        console.log('start snapshotting chat', phoneNumber);
+        this.chatSnapshotSub = this.fb.getUserChat(phoneNumber).onSnapshot((querySnaphost) => {
+            const chats = [] as any[];
+            console.log(querySnaphost.size);
+            querySnaphost.forEach(q => {
+                chats.push({
+                    key: q.data().from,
+                    from: q.data().from,
+                    to: q.data().to,
+                    direction: q.data().direction,
+                    message: q.data().message,
+                    ts: q.data().createdAt})
+            });
+            console.log(chats);
+            this.chat.next(chats);
+        })
+    }
 }
+
+
+/*
+private socket$: Observable<string> = new Observable<string>(
+  observer => {
+    this.socket.on('value', (data) => {
+      let v = data['value'];
+      observer.next(v);
+    });
+
+    return () => this.socket.disconnect();
+  })
+  .share();
+
+public getValue(): Observable<string> {
+  return this.socket$;
+}
+
+ */
--- src/inbox/inbox.tsx
+++ src/inbox/inbox.tsx
@@ -1,7 +1,10 @@
-import React, { useState} from 'react'
+import React, {useContext, useRef, useState} from 'react'
 import {Table, Input, Drawer} from "antd";
 import {columns, generateFakeData} from "./message-table-definition";
 import {useChats } from "../firebase/hook";
+import {MessageDirection} from "../firebase/data-context";
+import {DataContext} from "../PrivateZone";
+import {takeUntil, takeWhile} from "rxjs/operators";
 
 const { Search } = Input;
 
@@ -9,7 +12,11 @@ export const Inbox = () => {
     //const fb = useContext(FirebaseContext);
     const [isDrawerVisible, setDrawerVisible] = useState(false);
     const [selectedRow, setSelectedRow] = useState<any>(null);
+    const subs = useRef();
+
     const chats = useChats();
+
+    const data = useContext(DataContext); // make something similar as useChats
 
     return (
     <div className='inbox'>
@@ -19,10 +26,22 @@ export const Inbox = () => {
             size="large"
             onSearch={value => console.log(value)}/>
         <Table columns={columns}
-               onRow={(record) => ({
+               onRow={(record: any) => ({ // TODO go back to ITMessage
                    onClick: () => {
+                       console.log('row selected', record);
                        setSelectedRow(record);
+                       let number;
+                       if ((record as any).direction === MessageDirection.INBOUND) {
+                           number = record.from;
+                       } else {
+                           number = record.to;
+                       }
+                       data.startChatSnapshot(number);
                        setDrawerVisible(true);
+                       data.chat.pipe(takeWhile(() => isDrawerVisible))
+                           .subscribe((v) => {
+                               console.log('stuff', v);
+                            });
                    },
                })}
                dataSource={chats}

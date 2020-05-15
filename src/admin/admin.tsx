8003cdd2ab785e4b273dcefa1a812d8389367df7
Francois Chabbey <francois@streamsets.com>
2020-05-15 22:24:59 +0200

    Basic admin page

--- src/admin/admin.tsx
+++ src/admin/admin.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import FirebaseContext from "../firebase/context";
 
 export const Admin = () => {
 
+    const fb = useContext(FirebaseContext)!;
+
+
 
     return <div>
-        Admin section
+        <div style={{marginBottom:20}}>
+            <b> Manage my account</b>
+        </div>
+
+
+        <div><b> Login Info: </b></div>
+        <p>{
+            fb.currentUser!.email} <br/>
+            <a> Reset my password </a>
+        </p>
+
+
+        <div><b>Phone number associated with this account:</b></div>
+        <p>{fb.currentUser!.phoneNumber}</p>
+
+        <p><b> Plan: </b></p>
+
+        <p><b> Usage: </b></p>
     </div>
 }
